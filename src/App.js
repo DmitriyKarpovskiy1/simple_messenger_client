97c5f00cfebde2ad406407a39c15aff578fa2e61
Dmitriy Karpovskiy <dmitriy.karpovskiy@fruct.org>
2020-12-05 14:34:36 +0300

    Fix whitespaces

--- src/App.js
+++ src/App.js
@@ -9,32 +9,32 @@ class App extends Component {
   render() {
     return (
       <html lang="en">
-  <head>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></link>
-    <title>Simple Messager</title>
-  </head>
-  <body>
-  <div>
-       <Router basename={process.env.PUBLIC_URL}>
-      <div className="App">
-       <header className="App-header">
-         <div class="text-center">
-          <Link to="/"><h1>Simple Messenger</h1></Link>
-        </div>
-      </header>
-          <Switch>
-            <Route exact path='/' component={Main} />
-            <Route exact path='/chats' component={Chats} />
-            <Route exact path='/chat/:id' component={Chat} />
-          </Switch>
-      </div>
-    </Router>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-  </body>
-</html>
+        <head>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></link>
+          <title>Simple Messager</title>
+        </head>
+        <body>
+          <div>
+            <Router basename={process.env.PUBLIC_URL}>
+              <div className="App">
+                <header className="App-header">
+                  <div class="text-center">
+                    <Link to="/"><h1>Simple Messenger</h1></Link>
+                  </div>
+                </header>
+                <Switch>
+                  <Route exact path='/' component={Main} />
+                  <Route exact path='/chats' component={Chats} />
+                  <Route exact path='/chat/:id' component={Chat} />
+                </Switch>
+              </div>
+            </Router>
+          </div>
+          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+        </body>
+      </html>
     );
   }
 }
